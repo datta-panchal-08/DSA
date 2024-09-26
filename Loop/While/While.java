41463d3233d4af8252f083a61e83d7aeb3ed6bc7
Dattatray Panchal <dattapanchal464@gmail.com>
2024-09-26 18:59:32 +0530

    move to th while folder

---
+++ Loop/While/While.java
@@ -0,0 +1,11 @@
+package While;
+
+public class While {
+    public static void main(String args[]) {
+        int num = 1;
+        while (num <= 10) {
+            System.out.println("numbers : " + num);
+            num++;
+        }
+    }
+}
